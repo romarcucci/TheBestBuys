443d9d1c9f68a79c678b1f7d7c1ef243f04a337f
romarcucci <romain.marcucci@gmail.com>
2024-05-06 00:31:33 +0200

    remove Product recommendation component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import ProductGrid from './components/ProductGrid';
 import ChatbotInterface from './components/ChatbotInterface';
-import ProductRecommendations from './components/ProductRecommendations'; // Importing ProductRecommendations
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
-  const [recommendations, setRecommendations] = useState([]); // State to hold product recommendations
 
   const handleCategorySelect = (category) => {
     setSelectedCategory(category);
-  };
-
-  const handleRecommendations = (products) => {
-    setRecommendations(products); // Update the recommendations state when new products are received
   };
 
   return (
@@ -25,17 +19,7 @@ const App = () => {
         <ProductGrid onCategorySelect={handleCategorySelect} />
       )}
       {selectedCategory && (
-        <>
-          {/* Display recommendations above the chatbot interface */}
-          <ProductRecommendations
-            products={recommendations}
-            reset={() => setSelectedCategory(null)}
-          />
-          <ChatbotInterface
-            selectedCategory={selectedCategory}
-            onRecommendations={handleRecommendations} // Pass the handler to receive recommendations
-          />
-        </>
+        <ChatbotInterface selectedCategory={selectedCategory} />
       )}
     </div>
   );
