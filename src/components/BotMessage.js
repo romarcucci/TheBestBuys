5f2401523e5872d6988faa55529bb66a17407b2c
romarcucci <romain.marcucci@gmail.com>
2024-05-09 16:53:28 +0200

    BotMessage

--- src/components/BotMessage.js
+++ src/components/BotMessage.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import robotAvatar from '../assets/robot_profile_picture.png'; // Path to your robot avatar
-import '../styles/BotMessage.scss'; // SCSS file for styling
-import '../styles/ChatbotInterface.scss';
+import robotAvatar from '../assets/robot_profile_picture.png';
+import '../styles/BotMessage.scss';
 
-const BotMessage = ({ className, content, recommendations }) => {
+const BotMessage = ({ content }) => {
   return (
-    <div className={`bot-message ${className}`}>
-      {' '}
-      {/* Apply new class for structure */}
-      <div className="message-row">
+    <div>
+      <div className={'chat-msg bot-message'}>
         <img src={robotAvatar} alt="Bot" className="profile-picture" />
         <div className="message-content">{content}</div>
       </div>
-      {/* Display additional recommendations as tiles below the main bot message */}
-      {recommendations && recommendations.length > 0 && (
-        <div className="recommendations-row">
-          {recommendations.map((rec, index) => (
-            <div
-              key={index}
-              className="recommendation-tile"
-              dangerouslySetInnerHTML={{ __html: rec }} // Insert raw HTML
-            />
-          ))}
-        </div>
-      )}
     </div>
   );
 };
