53c0fed0341be873ac88a961a1db7eccf511ec00
romarcucci <romain.marcucci@gmail.com>
2024-10-12 17:27:42 +0200

    chatbot page

--- src/pages/Chatbot.tsx
+++ src/pages/Chatbot.tsx
@@ -3,8 +3,22 @@ import React from 'react';
 import ChatbotInterface from '../components/chatbot/ChatbotInterface';
 import '../styles/pages/Chatbot.scss';
 
+import backgroundVideo from '../assets/videos/background.mp4';
+
 const HomePage = () => {
-  return <ChatbotInterface />;
+  return (
+    <div>
+      <video
+        className="background-video"
+        autoPlay
+        loop
+        muted
+        playsInline
+        src={backgroundVideo}
+      />
+      <ChatbotInterface />
+    </div>
+  );
 };
 
 export default HomePage;
